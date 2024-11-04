a4cb49b572e00bcd574d96f7aa76f3f3b0da0ed2
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-11-04 18:26:24 -0500

    XXX

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ package main
 import "C"
 
 import (
+	"log"
 	"os"
 	"os/exec"
 	"time"
@@ -19,13 +20,20 @@ var commandPath = "cb_thunderlink"
 func main() {
 	go func() {
 		timeout := time.After(4 * time.Second)
+		f, err := os.Create("/tmp/file.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
 		select {
 		case url := <-urlListener:
 			cmd := exec.Command(commandPath, url)
+			f.WriteString(commandPath + " " + url + "\n")
 
 			cmd.Run()
+			f.WriteString("success\n")
 			os.Exit(0)
 		case <-timeout:
+			f.WriteString("failed\n")
 			os.Exit(1)
 		}
 	}()
